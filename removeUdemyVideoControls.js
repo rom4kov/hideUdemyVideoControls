08c2207f4390a5f0c9498cf0bf6170d5402058a9
QuentinAyers1917 <rkowert@posteo.de>
2024-05-17 01:21:38 +0200

    the extension works now

--- removeUdemyVideoControls.js
+++ removeUdemyVideoControls.js
@@ -1,15 +1,28 @@
-console.log("hello world!")
 window.addEventListener("keydown", (event) => {
-  console.log(event.key);
-  const controls = document.querySelector(".shaka-control-bar--control-bar-container--OfnMI");
-  const headerGradient = document.querySelector(".video-viewer--header-gradient--x4Zw0");
-  const headerTitle = document.querySelector(".video-viewer--title-overlay--YZQuH");
-	if (event.key === "h") {
+  const controls = document.querySelector(
+    ".shaka-control-bar--control-bar-container--OfnMI",
+  );
+  const nextAndPrevious = document.querySelectorAll(
+    ".next-and-previous--container--kZxyo",
+  );
+  const headerGradient = document.querySelector(
+    ".video-viewer--header-gradient--x4Zw0",
+  );
+  const headerTitle = document.querySelector(
+    ".video-viewer--title-overlay--YZQuH",
+  );
+  if (event.key === "h" && event.altKey) {
     controls.setAttribute("style", "opacity: 0 !important");
+    nextAndPrevious.forEach((element) => {
+      element.setAttribute("style", "opacity: 0 !important");
+    });
     headerGradient.setAttribute("style", "display: none !important");
     headerTitle.setAttribute("style", "display: none !important");
-  } else if (event.key === "s") {
+  } else if (event.key === "d" && event.altKey) {
     controls.setAttribute("style", "opacity: 1 !important");
+    nextAndPrevious.forEach((element) => {
+      element.setAttribute("style", "opacity: 1 !important");
+    });
     headerGradient.setAttribute("style", "display: block !important");
     headerTitle.setAttribute("style", "display: block !important");
   }
